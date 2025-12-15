f6ea299123e25bf9e9bd0ec62650f3f4da1e0ea0
Mustaqeem Borkar <mustaqeemborkar59@gmail.com>
2025-12-15 09:43:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "sheetmaster-woo4-3652307-f6517",
+  "appId": "1:1052692277102:web:22cc1f34943c206112037f",
+  "apiKey": "AIzaSyDrl4GEHOUjWt1Hr8L0KTjbIhZxRbu-i6Y",
+  "authDomain": "sheetmaster-woo4-3652307-f6517.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1052692277102"
+};
