2027aed1c1b9c9dd93692fc42c6cb52e6d63bd35
Mustaqeem Borkar <mustaqeemborkar59@gmail.com>
2025-12-16 04:32:10 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Firebase Admin c

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -17,7 +17,7 @@ function initializeAdminApp(): App {
 
   const serviceAccount = {
       type: "service_account",
-      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
       private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
