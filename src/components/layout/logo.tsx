69fc92536ed401b8c234409a5608cfb1c12d09f6
Mustaqeem Borkar <mustaqeemborkar59@gmail.com>
2025-12-17 12:17:02 +0000

    Solutions ki size chhota karo

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -7,9 +7,9 @@ export function Logo() {
       href="/"
       className="flex items-center text-xl font-semibold font-headline hover:text-primary/90 transition-colors"
     >
-      <span className="whitespace-nowrap">
-        <span className="text-foreground">Yasir Sofware </span>
-        <span className="text-primary">Solutions</span>
+      <span className="whitespace-nowrap flex items-baseline gap-x-1">
+        <span className="text-foreground">Yasir Sofware</span>
+        <span className="text-primary text-lg">Solutions</span>
       </span>
     </Link>
   );
