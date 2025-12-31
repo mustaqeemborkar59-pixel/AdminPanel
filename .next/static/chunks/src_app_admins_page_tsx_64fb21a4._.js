a8cff5ed6a191daca041649c3e4f2d6b325c02b8
Mustaqeem Borkar <mustaqeemborkar59@gmail.com>
2025-12-31 10:48:16 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- .next/static/chunks/src_app_admins_page_tsx_64fb21a4._.js
+++ .next/static/chunks/src_app_admins_page_tsx_64fb21a4._.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_admins_page_tsx_64fb21a4._.js",
   "chunks": [
-    "static/chunks/src_c5d14b2a._.js",
-    "static/chunks/node_modules_e66ba4f9._.js"
+    "static/chunks/src_d58a94b8._.js",
+    "static/chunks/node_modules_12e13982._.js"
   ],
   "source": "dynamic"
 });
